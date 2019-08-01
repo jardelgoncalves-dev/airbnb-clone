1ff88e99d61b4678e98ba323da8bc092233110f1
Jardel <jardelgoncalves1996@gmail.com>
2019-08-01 16:57:30 -0300

    controller responsável pela autenticação

---
+++ backend/app/Controllers/Http/SessionController.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class SessionController {
+  async create ({ request, auth }) {
+    const { email, password } = request.all()
+
+    const token = await auth.attempt(email, password)
+
+    return token
+  }
+}
+
+module.exports = SessionController
