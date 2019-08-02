99edf16426a08a06af6649644b0de125ef1185e1
Jardel <jardelgoncalves1996@gmail.com>
2019-08-01 22:45:13 -0300

    add relacionamento 1:n com property

--- backend/app/Models/User.js
+++ backend/app/Models/User.js
@@ -34,6 +34,10 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  properties () {
+    this.hasMany('App/Models/Property')
+  }
 }
 
 module.exports = User
