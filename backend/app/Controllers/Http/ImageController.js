1506562bcc54a6f4431a3c8dc24c37261c01d425
Jardel <jardelgoncalves1996@gmail.com>
2019-08-02 01:39:36 -0300

    add imageController para armazenar e exibir imagens

---
+++ backend/app/Controllers/Http/ImageController.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Helpers = use('Helpers')
+
+const Property = use('App/Models/Property')
+const Image = use('App/Models/Image')
+
+class ImageController {
+  async show ({ params, response }) {
+    return response.download(Helpers.tmpPath(`uploads/${params.path}`))
+  }
+  
+  async store ({ params, request }) {
+    const property = await Property.findOrFail(params.id)
+
+    const images = request.file('image', {
+      types: ['image'],
+      size: '2mb'
+    })
+
+    await images.moveAll(Helpers.tmpPath('uploads'), file => {
+      name: `${Date.now()}-${file.clientName}`
+    })
+
+    if (!images.movedAll()) {
+      return images.errors()
+    }
+
+    await Promise.all(
+      images
+        .movedList()
+        .map(image => property.images().create({ path: image.fileName }))
+    )
+  }
+}
+
+module.exports = ImageController
