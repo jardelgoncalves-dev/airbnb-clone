087400d109378b1050531cc4163e14243036e1de
Jardel <jardelgoncalves1996@gmail.com>
2019-08-02 00:58:34 -0300

    add rotas para /properties

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -18,3 +18,6 @@ const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
 Route.post('/session', 'SessionController.create')
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
