4e90bf8fd7b8e1f7ff92b4568097060272a74d2f
Jardel <jardelgoncalves1996@gmail.com>
2019-08-02 00:58:05 -0300

    add query que retorna imoveis a uma determinada distancia (usando haversine)

--- backend/app/Models/Property.js
+++ backend/app/Models/Property.js
@@ -3,7 +3,23 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
+const Database = use('Database')
+
 class Property extends Model {
+  
+  static scopeNearBy (query, latitude, longitude, distance) {
+    const haversine = `(6371 * acos(cos(radians(${latitude}))
+      * cos(radians(latitude))
+      * cos(radians(longitude)
+      - radians(${longitude}))
+      + sin(radians(${latitude}))
+      * sin(radians(latitude))))`
+
+    return query
+      .select('*', Database.raw(`${haversine} as distance`))
+      .whereRaw(`${haversine} < ${distance}`)
+  }
+
   user () {
     this.belongsTo('App/Models/User')
   }
