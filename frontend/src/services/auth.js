deb47a816282880897c55200322c3d2be980ded2
Jardel <jardelgoncalves1996@gmail.com>
2019-08-03 15:24:25 -0300

    metodos para salvar, remover e obter o token do usuario

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,12 @@
+const TOKEN_KEY = '@airbnb/token'
+export const logout = () => {
+  localStorage.removeItem(TOKEN_KEY)
+}
+
+export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null
+
+export const getToken = () => localStorage.getItem(TOKEN_KEY)
+
+export const login = token => {
+  localStorage.setItem(TOKEN_KEY, token)
+}
