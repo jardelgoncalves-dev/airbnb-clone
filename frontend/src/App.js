4bbc4235d2f8c8b9c25fd45a1aec7142137247eb
Jardel <jardelgoncalves1996@gmail.com>
2019-08-03 15:23:58 -0300

    chamando o estilo global

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Routes from './routes'
+
+import GlobalStyle from './styles/global'
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <div>
+    <GlobalStyle />
+    <Routes />
+  </div>
 }
 
 export default App;
