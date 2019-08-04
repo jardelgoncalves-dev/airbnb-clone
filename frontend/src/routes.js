eb070b07ce599224c8cb6534e328133057733814
Jardel <jardelgoncalves1996@gmail.com>
2019-08-03 22:47:50 -0300

    add modalContainer e fragment

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { ModalContainer } from "react-router-modal";
 import ProtectedRoute from './ProtectedRoute'
+import "react-router-modal/css/react-router-modal.css";
 
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
@@ -8,12 +10,15 @@ import App from './pages/App'
 
 const Routes = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={SignIn} />
-      <Route path="/signup" component={SignUp} />
-      <ProtectedRoute path="/app" component={App} />
-      <Route path="*" component={() => <h1>Not found</h1>} />
-    </Switch>
+  <Fragment>
+      <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <ProtectedRoute path="/app" component={App} />
+        <Route path="*" component={() => <h1>Not found</h1>} />
+      </Switch>
+      <ModalContainer />
+    </Fragment>
   </BrowserRouter>
 )
 
